5d6a3e03611d7ad25c3c02b1521254f1f7a093f4
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-14 13:14:19 -0700

    Got a homepage rendering as a sanity check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
+import Layout from '@/components/Layout';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      I am a page. Shut up, yes I am.
-    </main>
-  );
+  return <Layout>I am a page. Shut up, yes I am.</Layout>;
 }
