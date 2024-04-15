bd6861e12da8468e9c4037aa1878fdc6064c69aa
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-14 17:48:50 -0700

    Added some formatting + an About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Image from 'next/image';
+
+const About = () => {
+  return (
+    <div className='max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
+      <h1 className='text-4xl font-bold mb-8 text-center'>
+        About Annie Hetrick-Dahm
+      </h1>
+      <div className='flex flex-col md:flex-row gap-8'>
+        <div className='md:w-1/2'>
+          <Image
+            src='/harmony.jpeg'
+            alt='Annie Hetrick-Dahm'
+            width={400}
+            height={500}
+            className='w-full h-auto rounded-lg shadow-md'
+          />
+        </div>
+        <div className='md:w-1/2'>
+          <p className='mb-6 text-lg leading-relaxed'>
+            Annie Hetrick-Dahm is a 30-year-old from Los Angeles navigating the
+            worlds of architecture and art every day. Architecture provides
+            stability amidst chaos, while painting serves as a sanctuary for
+            emotional exploration.
+          </p>
+          <p className='mb-6 text-lg leading-relaxed'>
+            With a passion for storytelling, Annie examines themes of
+            femininity, nature, and architecture. She aims to evoke wonder and
+            introspection in the viewer through vivid colors, optical art, and
+            surrealism. Her fascination with op art adds an intriguing dimension
+            to her work, challenging perceptions and inviting viewers to see the
+            world through a new lens.
+          </p>
+          <p className='mb-6 text-lg leading-relaxed'>
+            Blurring reality and imagination, her paintings explore the
+            complexities of the mind, revealing resilience and vulnerability.
+            Her work often explores the nuances of the female experience,
+            capturing both its strength and vulnerability with grace and
+            sensitivity.
+          </p>
+          <p className='mb-6 text-lg leading-relaxed'>
+            Annie&apos;s work reflects her inner thoughts, inviting
+            self-discovery. Passionate about art, she seeks to inspire and
+            provoke conversations, pursuing opportunities like grants,
+            residencies, and gallery showcases.
+          </p>
+          <p className='text-lg leading-relaxed'>
+            Annie is also the creator and designer of Pop Architecture: a brand
+            of pop-art and architectural products for the everyday. Part art and
+            architecture, Pop Architecture is the one-stop shop for any designer
+            or appreciator to elevate their basics.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,42 +3,21 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className='max-w-6xl mx-auto px-6'>
-      <div className='py-6 border-t border-gray-100 text-center flex flex-col md:flex-row items-center justify-between'>
-        <p className='text-gray-600 text-sm'>
-          Powered by
-          <a
-            href='https://headlessdropshipping.com'
-            title='Learn more about how this site was made'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='ml-0.5 text-gray-800 hover:text-blue-600'
-          >
-            Headless Dropshipping Starter
-          </a>
-          , Built by{' '}
-          <a
-            href='https://twitter.com/notrab'
-            title='Follow the creator on Twitter'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='ml-0.5 text-gray-800 hover:text-blue-600'
-          >
-            @notrab
-          </a>
-        </p>
-        <nav className='flex items-center justify-end space-x-3 md:space-x-6'>
-          <Link href='/about' legacyBehavior>
-            <a className='text-gray-800 hover:text-blue-600 p-1 transition text-sm'>
-              FAQS
-            </a>
-          </Link>
-          <Link href='/terms-of-sale' legacyBehavior>
-            <a className='text-gray-800 hover:text-blue-600 p-1 transition text-sm'>
+    <footer className='px-6 flex justify-center'>
+      <div className='max-w-6xl py-6 w-full border-t border-gray-100 text-center flex flex-col md:flex-row items-center justify-between'>
+        <div className='text-center flex items-center w-full justify-between'>
+          <p className='text-gray-500 text-sm'>
+            Powered by wonder, built by coffee, and inspired by the world.
+          </p>
+          <nav className='flex items-center justify-between md:space-x-6'>
+            <Link
+              href='/terms-of-sale'
+              className='text-gray-600 hover:text-blue-600 p-1 transition text-sm'
+            >
               Terms of Sale
-            </a>
-          </Link>
-        </nav>
+            </Link>
+          </nav>
+        </div>
       </div>
     </footer>
   );
